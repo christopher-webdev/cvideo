e6e0ce817dc293729ce9befaf8992aaca9c1c234
christopher <christopher_webdev@outlook.com>
2024-08-13 06:31:26 +0100

    Initial commit

---
+++ models/Admin.js
@@ -0,0 +1,26 @@
+// const mongoose = require('mongoose');
+
+// const AdminSchema = new mongoose.Schema({
+//     email: { type: String, unique: true },
+//     password: {
+//         type: String,
+//         required: true,
+//     },
+// });
+
+// module.exports = mongoose.model('Admin', AdminSchema);
+const mongoose = require('mongoose');
+
+const AdminSchema = new mongoose.Schema({
+    email: { type: String, unique: true },
+    password: {
+        type: String,
+        required: true,
+    },
+    isSuperuser: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+module.exports = mongoose.model('Admin', AdminSchema);
